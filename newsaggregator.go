0582ec9b24b60ca79da113c081e8cdb984510bf5
Leo <leorent@provideit.se>
2019-04-19 16:18:51 +0200

    parse xml to a slice

--- newsaggregator.go
+++ newsaggregator.go
@@ -2,12 +2,28 @@ package main
 
 import ("fmt"
 "net/http"
-"io/ioutil")
+"io/ioutil"
+"encoding/xml")
 
-func main(){
+type SitemapIndex struct {
+	Locations[]Location `xml:"sitemap"`
+}
+
+type Location struct {
+	Loc string `xml:"loc"`
+}
+
+func (l Location) String() string {
+	return fmt.Sprintf(l.Loc)
+}
+
+func main() {
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
 	bytes, _ := ioutil.ReadAll(resp.Body)
-	stringBody := string(bytes)
-	fmt.Println(stringBody)
 	resp.Body.Close()
+
+	var s SitemapIndex
+	xml.Unmarshal(bytes, &s)
+
+	fmt.Println(s.Locations)
 }
