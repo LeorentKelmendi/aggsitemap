8155e4debf130c4af9265a859b94249584a2bda7
Leo <leorent@provideit.se>
2019-04-19 16:06:36 +0200

    get sitemap.xml via http get

--- newsaggregator.go
+++ newsaggregator.go
@@ -1,9 +1,13 @@
 package main
 
 import ("fmt"
-"net/http")
-
+"net/http"
+"io/ioutil")
 
 func main(){
-
+	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml")
+	bytes, _ := ioutil.ReadAll(resp.Body)
+	stringBody := string(bytes)
+	fmt.Println(stringBody)
+	resp.Body.Close()
 }
